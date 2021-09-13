ee033c0fa76a4de7380de29806d7533ce7761fdb
Marina8989 <marina.mill209@gmail.com>
2021-09-13 16:05:57 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+const Button = () => {
+   return (
+       <button>Click</button>
+   )
+}
+
+const Header = (props) => {
+   return(
+      <div>
+        <h2>{props.name}</h2>
+        <h5>{props.number}</h5>
+      </div>  
+   )
+}
 class App extends React.Component{
     render(){
         return(
-            <h2>hello</h2>
+           <> 
+            <h2>React Test</h2>
+            <Header name={'Marina'} number={32}/>
+            <Button />
+           </>
         )
     }
 }
