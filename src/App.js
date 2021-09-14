461ab4473754a356fa3581ba6d05aa46ccd71cf3
Marina8989 <marina.mill209@gmail.com>
2021-09-13 19:43:05 -0700

    react

--- src/App.js
+++ src/App.js
@@ -16,6 +16,21 @@ const InputBox = (props) => {
        <input type="text" placeholder={props.placeholder} style={{border: '1px solid lightblue', borderRadius: '3px', margin: '.2rem 0'}}/>
     )
 }
+
+const CheckBox = () => {
+    return(
+       <>
+         <input type="checkbox" id="check" name="check" style={{margin: '.5rem .2rem'}}/>
+         <label htmlFor="check">I agree to the Terms and Privacy Policy</label>
+       </>
+    )
+}
+
+const Sign = (props) => {
+   return (
+      <button style={{color: 'green'}}>{props.btnText}</button>
+   )
+}
 class App extends React.Component{
     render(){
         return(
@@ -25,7 +40,10 @@ class App extends React.Component{
             <Text  text={'or use email for credentials'}/>
             <InputBox placeholder={'Name'} /><br />
             <InputBox placeholder={'Email'} /><br />
-            <InputBox placeholder={'Password'} />
+            <InputBox placeholder={'Password'} /><br />
+            <CheckBox /><br />
+            <Sign btnText={'Sign Up'} />
+            <Sign btnText={'Sign In'} />
            </div>
         )
     }
