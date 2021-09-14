95b0b7db88711802b9325ef0712889e41a4b2f81
Marina8989 <marina.mill209@gmail.com>
2021-09-13 19:33:21 -0700

    react

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React from 'react';
 
-const Button = (props) => {
-   return (
-       <button>{props.text}</button>
+const Title = (props) => {
+   return(
+      <h1>{props.title}</h1>
+   )
+}
+const Text = (props) => {
+   return(
+      <h6>{props.text}</h6>
    )
 }
 
-const Header = (props) => {
-   return(
-      <div>
-        <h2>{props.name}</h2>
-        <h5>{props.number}</h5>
-        <Button text={props.buttonText}/>
-      </div>  
-   )
+const InputBox = (props) => {
+    return (
+       <input type="text" placeholder={props.placeholder} style={{border: '1px solid lightblue', borderRadius: '3px', margin: '.2rem 0'}}/>
+    )
 }
 class App extends React.Component{
     render(){
         return(
-           <> 
-            <h2>React Test</h2>
-            <Header name={'Marina'} number={32} buttonText={'Log In'}/>
-            <Header name={'Albert'} number={35} buttonText={'Log Out'}/>
-           </>
+           <div style={{textAlign: 'center', margin: 'auto'}}> 
+            <Title title={'Create Account'} />
+            <h6 style={{ margin: 'auto', border: '1px solid lightblue', borderRadius: '2px', width: '120px'}}>one two three four</h6>
+            <Text  text={'or use email for credentials'}/>
+            <InputBox placeholder={'Name'} /><br />
+            <InputBox placeholder={'Email'} /><br />
+            <InputBox placeholder={'Password'} />
+           </div>
         )
     }
 }
