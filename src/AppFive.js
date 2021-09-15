4b84e4c3b4e44be29fb5a2658676dab3936bedaf
Marina8989 <marina.mill209@gmail.com>
2021-09-15 13:18:45 -0700

    react

--- src/AppFive.js
+++ src/AppFive.js
@@ -1,21 +1,5 @@
 import React from 'react';
 
-//class Counter extends React.Component{
-    // state={
-    //     counter: this.props.defaultCounterValue
-    // }
-    // handleIncrement = () => {
-    //   this.setState({counter: this.state.counter + this.props.increment})
-    // }
-    //render(){
-       // return(
-       //  <div>
-          //  {this.props.counters.map(counter => <h4>{counter.defaultValue}</h4>)}
-          // <button onClick={this.handleIncrement}>increment</button>
-        // </div>  
-        //)
-   // }
-//}
 
 class AppFive extends React.Component{
     state={
@@ -45,7 +29,7 @@ class AppFive extends React.Component{
        const newCount = this.state.counters.map(el => {
            if(el.id === item.id) {
              if(!el.increment) {
-               el.defaultValue+=1;
+               el.defaultValue+=17;
            }else{
                el.defaultValue+=el.increment;
            }
@@ -59,10 +43,10 @@ class AppFive extends React.Component{
           <>  
            {this.state.counters.map(elem => {
                return (
-                   <div key={elem.id}>
+                   <div>
                       <h4>{elem.defaultValue}</h4>
-                      <button onClick={() => this.handleIncrement(elem)}>increment</button>
-                   </div>
+                      <button onClick={()=> this.handleIncrement(elem)}>increment</button>
+                    </div>
                )
            })}
          </>  
