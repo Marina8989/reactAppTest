93cd4b5f7fd9d280a2286bc4095ddae566808d6a
Marina8989 <marina.mill209@gmail.com>
2021-09-16 15:37:01 -0700

    react

--- src/AppFetch.js
+++ src/AppFetch.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class AppFetch extends React.Component{
@@ -10,23 +11,25 @@ class AppFetch extends React.Component{
     }
 
     getUser = async (user) => {
-       this.setState({isLoading: !this.state.isLoading});
+       this.setState({isLoading: true});
        try{
          const {data} = await axios(`https://api.github.com/users/${user}`);
          const newList = [...this.state.list, data];
-         this.setState({list: newList})
+         this.setState({list: newList, isLoading: false});
+         console.log(this.state.list)
        }catch(err) {
          console.log(err)
+         this.setState({hasError: true})
        }
     }
+    handleChange = (e) => {
+      this.setState({searchInput: e.target.value});
+    }
 
-    handleSubmit = (value) => {
-     const item = {
-         id: `${Math.random() * 20}`,
-         value
-     }
-     const newList = [...this.state.list, item];
-     this.setState({list: newList});
+    handleSubmit = (e) => {
+       e.preventDefault();
+       this.getUser(this.state.searchInput);
+       this.setState({searchInput: ''})
     }
     render() {
         return(
@@ -34,6 +37,16 @@ class AppFetch extends React.Component{
                 <form onSubmit={this.handleSubmit}>
                     <input value={this.state.searchInput} onChange={this.handleChange}/>
                 </form>
+                {this.state.list.map(item => {
+                    const {avatar_url, name, id} = item;
+                    return(
+                        <div key={id}>
+                          <h4>{name}</h4>
+                          <img src={avatar_url} alt={'logo'} style={{width: '120px'}}/>
+                        </div>
+                    )
+                }) }
+                {this.state.hasError && <h3>Error... try again</h3>}
             </div>
         )
     }
