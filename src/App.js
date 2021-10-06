9d968a27d903e607527ef68ef9b516ca8bb501bb
Marina8989 <marina.mill209@gmail.com>
2021-10-05 20:58:28 -0700

    react router parctice

--- src/App.js
+++ src/App.js
@@ -1,82 +1,45 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Input = styled.input`
-    border: 1px solid green;
-    margin: .5rem 0;
-`
-const Container = styled.div`
-    margin: auto;
-    text-align: center;
-`
-
-class Form extends React.Component{
-    state={
-        inputValue: ''
-    }
-    handleChange = (e) => {
-      this.setState({inputValue: e.target.value})
-    }
-    handleSubmit = (e) => {
-      e.preventDefault();
-      this.props.handleSubmit(this.state.inputValue);
-      this.setState({inputValue: ''});
-    }
-    render(){
-        return(
-            <form onSubmit={this.handleSubmit}>
-                <Input value={this.state.inputValue} onChange={this.handleChange} />
-            </form>
-        )
-    }
-}
-
-const Button = (props) => {
-    return(
-        <button onClick={() => props.handleClick(props.item)}>{props.title}</button>
-    )
-}
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 class App extends React.Component{
-    state={
-        list: []
-    }
-    handleSubmit = (value) => {
-        const item = {
-            id: `${Math.random() * 20}`,
-            value,
-            toggle: false
-        }
-        const newList = [...this.state.list, item];
-        this.setState({list: newList});
-    }
-    handleClick = (item) => {
-      const newList = this.state.list.map(el => {
-          if(el === item) {
-             el.toggle = !el.toggle;
-          }
-          return el;
-      })
-      this.setState({list: newList});
-    }
-    render() {
-        console.log(this.state.list)
+    render(){
         return(
-            <Container>
-              <h3>ToDo List</h3>
-              <Form handleSubmit={this.handleSubmit}/>
-              {this.state.list.map(item => {
-                  const {id, value} = item
-                  return(
-                      <div>
-                        <h5 key={id} className={this.state.list.toggle ? 'toggle' : ''}>{value}</h5>
-                        <Button title="toggle" handleClick={this.handleClick}/>
-                      </div>
-                  )
-              })}
-            </Container>
+           <Router>
+               <div className='container'>
+                   <Link to='/'>Home</Link><br/>
+                   <Link to='/about'>About</Link><br/>
+                   <Link to='/dashboard'>Dashboard</Link><br/>
+               </div>
+            
+              <hr />
+
+              <Switch>
+                 <Route path='/' exact>
+                    <Home />
+                 </Route>
+                 <Route path='/about' exact>
+                    <About />
+                 </Route>
+                 <Route path='/dashboard' exact>
+                     <Dashboard />
+                 </Route>
+              </Switch>
+
+           </Router>
         )
     }
 }
 
 export default App
+
+function Home() {
+    return 'HOME SWEET HOME';
+}
+
+function About() {
+    return 'About mi vida';
+}
+
+function Dashboard() {
+    return 'Como se dice dashboard en Espanol?';
+}
