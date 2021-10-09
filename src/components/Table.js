dc80aa2d4d21d16071c27851a7beb5c9dee6ad0c
Marina8989 <marina.mill209@gmail.com>
2021-10-09 00:38:30 -0700

    react

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,6 +4,9 @@ import {FaStarHalfAlt} from 'react-icons/fa';
 
 const StyledTable = styled.table`
     padding: .9rem .7rem;
+    background: rgb(17, 17, 17);
+    margin: 1rem auto;
+    border-radius: 5px;
 `
 
 const Table = ({coins}) => {
@@ -13,28 +16,29 @@ const Table = ({coins}) => {
             <StyledTable>
               <thead>
                 <tr>
-                    <th style={{fontSize: '14px'}}>#</th>
-                    <th style={{fontSize: '14px'}}>Coin</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Price</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>1h%</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>24h%</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>7d%</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>24h Volume/Morket Cap</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Circulating/Total Supply</th>
-                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Last 7d</th>
+                    <th style={{fontSize: '16px', color: 'rgb(84,90,131)'}}>#</th>
+                    <th style={{fontSize: '14px', color: 'rgb(84,90,131)'}}>Coin</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>Price</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>1h%</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>24h%</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>7d%</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>24h Volume/Morket Cap</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>Circulating/Total Supply</th>
+                    <th style={{padding: '0 2rem', fontSize: '14px', color: 'rgb(84,90,131)'}}>Last 7d</th>
                 </tr>
               </thead>
              {coin.map((item, index )=> (
                 <tbody>
                   <tr key={item.id}>
-                    <td style={{padding: '1rem', fontSize: '10px'}}>{index + 1}</td>
+                    <td style={{padding: '.6rem', fontSize: '10px'}}>{index + 1}</td>
                     <td style={{fontSize: '13px', padding: '0'}}><img src={item.image} style={{width: '15px'}}/> {item.name}({item.symbol})</td>
                     <td style={{fontSize: '13px', padding: '0 1rem'}}>${(item.current_price).toFixed(2)}</td>
-                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_1h_in_currency).toFixed(1)}%</td>
-                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_24h).toFixed(1)}</td>
-                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_7d_in_currency).toFixed(1)}%</td>
-                    <td style={{fontSize: '13px', padding: '0 1rem'}}>${(item.total_volume / 1000000000)}</td>
-                    <td style={{fontSize: '13px', padding: '0 1rem'}}>${item.market_cap}</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>{(item.price_change_percentage_1h_in_currency).toFixed(1)}%</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>{(item.price_change_percentage_24h).toFixed(1)}</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>{(item.price_change_percentage_7d_in_currency).toFixed(1)}%</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>${(item.total_volume / 1000000000)}</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>${item.market_cap}</td>
+                    <td style={{fontSize: '13px', padding: '0 2rem'}}>test</td>
                   </tr>
                 </tbody>
              ))}
--- src/pages/CoinList.js
+++ src/pages/CoinList.js
@@ -6,12 +6,12 @@ import styled from 'styled-components';
 
 const StyledDiv = styled.div`
    width: 90%;
-   ${'' /* border: 1px solid red; */}
    margin: auto;
-   background: rgb(2,14,3);
+   background: rgb(17, 17, 17);
    border-radius: 10px;
    padding: 2rem 1rem;
    margin-top: 2rem;
+   margin-bottom: 2rem;
 `
 const StyledLink = styled(Link)`
    text-decoration: none;
@@ -20,8 +20,8 @@ const StyledLink = styled(Link)`
 
 const StyledCoinList = styled(Link)`
     background: rgb(37, 37, 37);
-    padding: .5rem 1rem;
-    margin-right: 1.3rem;
+    padding: .5rem 1.2rem;
+    margin-right: 1.6rem;
     border-radius: 10px;
     text-decoration: none;
     color: white;
@@ -36,6 +36,7 @@ const StyledH = styled.h2`
 const StyledInnerDiv = styled.div`
    background: rgb(37, 37, 37);
    border-radius: 10px;
+   padding-bottom: 1.7rem;
 `
 
 class CoinList extends React.Component{
@@ -44,7 +45,7 @@ class CoinList extends React.Component{
     }
     getCoinInfo = async() => {
        try{
-         const {data} = await axios('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d');
+         const {data} = await axios('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d');
          const newList = [...this.state.coins, data];
          this.setState({coins: newList});
        }catch(err){
