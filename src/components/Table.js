62637a7d1802cfb77f6f667c57789d822fec3faf
Marina8989 <marina.mill209@gmail.com>
2021-10-09 00:07:30 -0700

    react

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,39 +1,44 @@
 import React from 'react';
+import styled from 'styled-components';
 import {FaStarHalfAlt} from 'react-icons/fa';
+
+const StyledTable = styled.table`
+    padding: .9rem .7rem;
+`
 
 const Table = ({coins}) => {
    return(
       <> 
         {coins.map(coin => (
-            <table>
+            <StyledTable>
               <thead>
                 <tr>
-                    <th>#</th>
-                    <th>Coin</th>
-                    <th>Price</th>
-                    <th>1h%</th>
-                    <th>24h%</th>
-                    <th>7d%</th>
-                    <th>24h Volume/Morket Cap</th>
-                    <th>Circulating/Total Supply</th>
-                    <th>Last 7d</th>
+                    <th style={{fontSize: '14px'}}>#</th>
+                    <th style={{fontSize: '14px'}}>Coin</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Price</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>1h%</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>24h%</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>7d%</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>24h Volume/Morket Cap</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Circulating/Total Supply</th>
+                    <th style={{padding: '0 1rem', fontSize: '14px'}}>Last 7d</th>
                 </tr>
               </thead>
              {coin.map((item, index )=> (
                 <tbody>
                   <tr key={item.id}>
-                    <td><FaStarHalfAlt /> {index + 1}</td>
-                    <td><img src={item.image} style={{width: '15px'}}/> {item.name}({item.symbol})</td>
-                    <td>${(item.current_price).toFixed(2)}</td>
-                    <td>{(item.price_change_percentage_1h_in_currency).toFixed(1)}%</td>
-                    <td>{(item.price_change_percentage_24h).toFixed(1)}</td>
-                    <td>{(item.price_change_percentage_7d_in_currency).toFixed(1)}%</td>
-                    <td>${(item.total_volume / 1000000000)}</td>
-                    <td>${item.market_cap}</td>
+                    <td style={{padding: '1rem', fontSize: '10px'}}>{index + 1}</td>
+                    <td style={{fontSize: '13px', padding: '0'}}><img src={item.image} style={{width: '15px'}}/> {item.name}({item.symbol})</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>${(item.current_price).toFixed(2)}</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_1h_in_currency).toFixed(1)}%</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_24h).toFixed(1)}</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>{(item.price_change_percentage_7d_in_currency).toFixed(1)}%</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>${(item.total_volume / 1000000000)}</td>
+                    <td style={{fontSize: '13px', padding: '0 1rem'}}>${item.market_cap}</td>
                   </tr>
                 </tbody>
              ))}
-             </table>
+             </StyledTable>
         ))}
      </>    
    )
