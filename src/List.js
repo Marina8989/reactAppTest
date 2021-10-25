8920fc082fcd6ee3bfcd887daadce78376bc80f5
Marina8989 <marina.mill209@gmail.com>
2021-10-25 15:11:40 -0700

    react

--- src/List.js
+++ src/List.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import Button from './Button';
 
-const List = ({list}) => {
-   const num = 0;
+class List extends React.Component{
+   state={
+      num: 0
+   }
+   handleLeft = () => {
+      if(this.state.num < 0) {
+         this.setState({num: 0})
+      }else {
+         this.setState({num: this.state.num - 1})
+      }
+      console.log(this.state.num)
+   }
+   handleRight = () => {
+      if(this.state.num > this.props.list.length) {
+       this.setState({num: 0})
+      }else {
+         this.setState({num: this.state.num + 1})
+      }
+   }
+   render() {
+      //let num = this.state.num
      return(
       <div>
-        <h3>{list[num].name}</h3>
-        <h4>{list[num].price}</h4>
+        <h3>{this.props.list[this.state.num].name}</h3>
+        <h4>{this.props.list[this.state.num].price}</h4>
+        <Button text='<' handleClick={this.handleLeft}/>
+        <Button text='>' handleClick={this.handleRight} />
       </div>
      )
-   }
+   }  
+}
 
 
 export default List;
