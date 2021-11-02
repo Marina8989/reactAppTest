0a4de04b2145e69a274958f447ac0f576e998edc
Marina8989 <marina.mill209@gmail.com>
2021-11-01 18:48:37 -0700

    react

--- src/App.js
+++ src/App.js
@@ -4,23 +4,48 @@ import './index.css';
 
 class App extends React.Component{
     state={
-        list: data
+        list: data,
+    }
+    handleClick = (item) => {
+      const newList = this.state.list.filter(el => {
+          if(el.order === item.order) {
+              return item
+          }
+          return el
+      })
+      this.setState({list: newList})
     }
     render(){
-        console.log(this.state.list)
+        //console.log(this.state.list)
         return(
             <>
               <h2>Experience</h2>
               <div className='div'>
-              {this.state.list.map(item => {
-                  const {id, company} = item;
-                  return(
-                      <h5 key={id}>{company}</h5>
-                  )
-              })}
+              <div>
+                {this.state.list.map(item => {
+                    const {id, company, title, dates, duties, order} = item;
+                    return(
+                        <>
+                        <button key={id} onClick={() => this.handleClick(item)}>{company}</button>
+                           <h4 >{title}</h4>
+                           <h5>{dates}</h5>
+                           <h5>{duties}</h5>
+                           <h5>{company}</h5>
+                           </>
+                    )
+                })}
+              </div>
               <div>
                   {this.state.list.map((item) => {
-                      const {id, order, title, dates, duties, company} = item[value]
+                      const {id, title, dates, duties, company} = item.order;
+                      return (
+                          <div key={id}>
+                           <h4 >{title}</h4>
+                           <h5>{dates}</h5>
+                           <h5>{duties}</h5>
+                           <h5>{company}</h5>
+                          </div>
+                      )
                   })}
               </div>
               </div>
