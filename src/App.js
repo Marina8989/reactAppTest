ba7e031b6e2d4a7e6c8628ed41f2c9e8d4519249
Marina8989 <marina.mill209@gmail.com>
2021-11-03 14:09:44 -0700

    react

--- src/App.js
+++ src/App.js
@@ -3,40 +3,24 @@ import data from './data';
 import './index.css';
 
 class App extends React.Component{
-    state={
-        list: data,
-        value: 0
+    state ={
+        items: data,
+        count: 0
     }
-    handleClick = (item) => {
-       this.setState({...this.state.list, value: (item.order - 1)})
-    }
-    render(){
-        console.log(this.state.list)
-        console.log(this.state.value)
-        let value = this.state.value
-        const {title, dates, duties} = this.state.list[value];
-        return(
-            <>
-               <h3>Experience</h3>
-               {this.state.list.map((item, index) => {
-                   return(
-                       <button key={index} onClick={() => this.handleClick(item)}>{item.company}</button>
-                   )
-               })}
-               <>
-                 <div>
-                    <h3>{title}</h3>
-                    <h4>{dates}</h4>
-                    {duties.map((el, index )=> {
-                        return(
-                          <>
-                           <h3 key={index}>{el}</h3>
-                          </>
-                        )
-                    })}
-                 </div>
-               </>
-            </>
+     handleSubmit = (e) => {
+       e.preventDefault();
+       console.log('submited')
+     }
+
+    render() {
+      console.log(this.state.items)
+      return(
+            <div>
+                <h3>Tired of old Lorem Ipsum?</h3>
+                <form onSubmit={this.handleSubmit}>
+                   <input type='number' value={this.state.count} onChange={this.handleChange} />
+                </form>
+            </div>
         )
     }
 }
--- src/data.js
+++ src/data.js
@@ -1,40 +1,10 @@
 const data = [
-    {
-        id: 'rwyrwwt',
-        order: 3,
-        title: 'Full Stack Web Developer',
-        dates: 'December 2015 - Present',
-        duties: [
-            'Totdhe fsjhfshn sjdjfh fskffhs sdfssh jshfshf fshshs fsdhfsfsj fsff',
-            'Fsdhhf sjfhs dfgj sfjsjsf skfjskj ksjfsfj sjfkj ksjfds kjfsk ksjsj sdfj',
-            'sd jdae jjad akdjakfjut ijeriru surwunfs sfjsdj skdjweri smfsf ksjfsfjjf'
-        ],
-        company: 'TOMMY'
-    },
-    {
-        id: 'lkgdfkg',
-        order: 2,
-        title: 'Full Stack Engineer',
-        dates: 'May 2015 - December 2015',
-        duties: [
-            'Teoqeqorqr fsjhfshn sjdjfh fskffhs sdfssh jshfshf fshshs fsdhfsfsj fsff',
-            'Fsdhhf sjfhs dfgj sfjsjsf skfjskj ksjfsfj sjfkj ksjfds kjfsk ksjsj sdfj',
-            'sd jdae jjad akdjakfjut ijeriru surwunfs sfjsdj skdjweri smfsf ksjfsfjjf'
-        ],
-        company: 'BIGDROP'
-    },
-    {
-        id: 'ueiyue',
-        order: 1,
-        title: 'Engineering Intern',
-        dates: 'May 2014 - September 2015',
-        duties: [
-            'sjfhh fsjhfshn sjdjfh fskffhs sdfssh jshfshf fshshs fsdhfsfsj fsff',
-            'Fsdhhf sjfhs dfgj sfjsjsf skfjskj ksjfsfj sjfkj ksjfds kjfsk ksjsj sdfj',
-            'sd jdae jjad akdjakfjut ijeriru surwunfs sfjsdj skdjweri smfsf ksjfsfjjf'
-        ],
-        company: 'CUKER'
-    },
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd',
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd',
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd',
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd',
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd',
+    'sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd sffh jahfahf fhfhf kfdfdfjf ffj kdfkfjf fjdfjf kdjfkfj fjfdj fjskj fsjfsj dfjsdj akff jkdfjj ksjdfsj kfjskj sdjfksdj kjfksf skfjskjf skjksj kfjsd'
 ]
 
 export default data
