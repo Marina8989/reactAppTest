cd1500447103e56633339635f10128b45afbdae8
Marina8989 <marina.mill209@gmail.com>
2021-11-03 19:01:15 -0700

    react

--- src/App.js
+++ src/App.js
@@ -8,17 +8,32 @@ class App extends React.Component{
         count: 0
     }
      handleSubmit = (e) => {
-       e.preventDefault();
-       console.log('submited')
+      e.preventDefault();
+      console.log('clicked')
+      this.setState({items: data, count: this.state.count})
+      console.log(this.state.count)
+      
+    //   this.setState({items: data, count: e.target.value})
+    //   console.log(this.state.count)
+     } 
+     handleChange = (num) => {
+         console.log(num)
+         if(num < 0){
+           this.setState({...this.state, count: num + 1})
+         }else{
+           this.setState({...this.state, count: num})
+         }
+      
+      console.log(this.state.count)
      }
-
     render() {
-      console.log(this.state.items)
+      //console.log(this.state.items)
+      //console.log(this.state.count)
       return(
             <div>
                 <h3>Tired of old Lorem Ipsum?</h3>
                 <form onSubmit={this.handleSubmit}>
-                   <input type='number' value={this.state.count} onChange={this.handleChange} />
+                   <input type='number' value={this.state.count} onChange={() => this.handleChange(this.state.count)} />
                 </form>
             </div>
         )
